c4e317655be05c0949f17d4c38624fc43f671984
Niall Murphy <c17313343@mydit.ie>
2020-11-25 15:55:01 +0000

    part settings

--- app/src/main/java/com/niall/younote/Settings.java
+++ app/src/main/java/com/niall/younote/Settings.java
@@ -49,8 +49,6 @@ public class Settings extends AppCompatActivity {
             Log.w("USER", "No user found");
 
         }
-
-
             myRef.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
